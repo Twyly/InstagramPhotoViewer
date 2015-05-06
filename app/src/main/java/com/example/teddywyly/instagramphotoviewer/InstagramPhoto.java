30ba250b224a98c1f988fa2ac3c3e8147d89818e
Teddy Wyly <wylynout@gmail.com>
2015-05-05 22:35:59 -0400

    initial commit

---
+++ app/src/main/java/com/example/teddywyly/instagramphotoviewer/InstagramPhoto.java
@@ -0,0 +1,12 @@
+package com.example.teddywyly.instagramphotoviewer;
+
+/**
+ * Created by teddywyly on 5/5/15.
+ */
+public class InstagramPhoto {
+    public String username;
+    public String caption;
+    public String imageURL;
+    public int imageHeight;
+    public int likesCount;
+}
